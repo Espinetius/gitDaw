6204008ba04938dead80ead707c7388104dd1956
Espinetius <48967809+Espinetius@users.noreply.github.com>
2022-01-20 18:05:52 +0100

    herencia ej de CuentaCorriente

---
+++ Programacion IntelliJ ejercicios/Herencia/src/Titular.java
@@ -0,0 +1,16 @@
+public class Titular {
+    public String nombre;
+    public String apellidos;
+    public int edad;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+    public int getEdad() {
+        return edad;
+    }
+}
