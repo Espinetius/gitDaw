edcb5225697ce35dc916f63a33a1f709eb1745c7
Espinetius <48967809+Espinetius@users.noreply.github.com>
2022-02-16 23:32:09 +0100

    ajedrez dia 2 casa

--- Programacion IntelliJ ejercicios/Ajedrez/src/Pieza.java
+++ Programacion IntelliJ ejercicios/Ajedrez/src/Pieza.java
@@ -17,7 +17,5 @@ public abstract class Pieza {
 	public abstract String pintarPieza();
 
 	@Override
-	public String toString() {
-		return "Pieza -> " +color;
-	}
+	public abstract String toString();
 }
--- Programacion IntelliJ ejercicios/Ajedrez/src/Tablero.java
+++ Programacion IntelliJ ejercicios/Ajedrez/src/Tablero.java
@@ -2,21 +2,60 @@ public class Tablero {
 	Pieza tablero[][]=new Pieza[8][8];
 	public Tablero() {
 		tablero[0][0] = new Torre("negro");
-		tablero[1][0] = new Caballo("negro");
-		tablero[2][0] = new Alfil("negro");
-		tablero[3][0] = new Reina("negro");
-		tablero[4][0] = new Rey("negro");
-		tablero[5][0] = new Alfil("negro");
-		tablero[6][0] = new Caballo("negro");
-		tablero[7][0] = new Torre("negro");
+		tablero[0][1] = new Caballo("negro");
+		tablero[0][2] = new Alfil("negro");
+		tablero[0][3] = new Reina("negro");
+		tablero[0][4] = new Rey("negro");
+		tablero[0][5] = new Alfil("negro");
+		tablero[0][6] = new Caballo("negro");
+		tablero[0][7] = new Torre("negro");
 
-		tablero[7][7] = new Torre("blanco");
-		tablero[7][6] = new Caballo("blanco");
-		tablero[7][5] = new Alfil("blanco");
-		tablero[7][4] = new Reina("blanco");
-		tablero[7][3] = new Rey("blanco");
-		tablero[7][2] = new Alfil("blanco");
+		tablero[7][0] = new Torre("blanco");
 		tablero[7][1] = new Caballo("blanco");
-		tablero[7][]
+		tablero[7][2] = new Alfil("blanco");
+		tablero[7][3] = new Reina("blanco");
+		tablero[7][4] = new Rey("blanco");
+		tablero[7][5] = new Alfil("blanco");
+		tablero[7][6] = new Caballo("blanco");
+		tablero[7][7] = new Torre("blanco");
 	}
+	public void pintartablero() {
+		for (int i = 0; i < 8; i++) {
+			for (int j = 0; j < 8; j++) {
+				if (tablero[i][j]!=null) {
+					System.out.print(tablero[i][j].toString());
+				} else {
+					System.out.println(" ");
+				}
+			}
+			System.out.println();
+		}
+	}
+	public boolean hayPieza(int fila, int columna) {
+		boolean respuesta = false;
+		boolean salida = false;
+		for (int i = 0; i < 8 && !salida; i++) {
+			for (int j = 0; j < 8 && !salida; j++) {
+				if (tablero[fila][columna]!=null) {
+					respuesta = true;
+					salida = true;
+				}
+			}
+		}
+		return respuesta;
+	}
+	public boolean hayPieza(Posicion pos) {
+		boolean respuesta =false;
+		boolean salida = false;
+		for (int i = 0; i < 8 && !salida; i++) {
+			for (int j = 0; j < 8 && !salida; j++) {
+				if (tablero[pos.getFila()][pos.getColumna()]!=null) {
+					respuesta=true;
+					salida=true;
+				}
+			}
+		}
+		return respuesta;
+	}
+
 }
